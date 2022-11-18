977ddfcfd78200457d121c11445fb9e267b5144e
Joel Rivera <joelito1408.04@gmail.com>
2022-11-17 19:23:17 -0500

    Index OK

--- Vistacontrol/Index.java
+++ Vistacontrol/Index.java
@@ -1,5 +1,16 @@
 package Vistacontrol;
 //Autor: Joel Rivera
 public class Index{
-
+	public static void crear(){
+	
+	
+	}
+	public static void editar(){
+	
+	
+	}
+	public static void eliminar(){
+	
+	
+	}
 }
